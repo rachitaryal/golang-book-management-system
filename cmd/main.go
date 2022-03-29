60ec663e40b5f1b1adc2b925d6d6cd47b2d81c6f
rachit <rachit.aryal01@gmail.com>
2022-03-29 20:15:41 +0545

    crud added in model

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/rachitaryal/golang-book-management-system/pkg/routes"
+)
+
+func main(){
+	router := mux.NewRouter()
+	routes.RegisterBookRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("localhost:9000", router))
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -20,3 +20,28 @@ func init(){
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func(book *Book) CreateBook() *Book{
+	db.NewRecord(book)
+	db.Create(&book)
+	return book
+}
+
+func GetAllBooks()[]Book{
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB){
+	var book Book
+	db :=db.Where("ID=?", Id).Find(&book)
+	return &book, db
+}
+
+func DeleteBookById(Id int64)Book{
+	var book Book
+	db.Where("ID=?", Id).Delete(&book)
+	return book
+
+}
